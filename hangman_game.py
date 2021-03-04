ef57621117f83980bd1d2dfd496656d95061b206
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2021-03-04 12:29:46 +0530

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -1,32 +1,29 @@
 import random
-import image
+from  image import IMAGES
 name = input("What is your name? ")
 print("Good Luck ! ", name)
  
-words = ['jyoti', 'chauhan', 'programming', 'coding', 
-         'python', 'mohini', 'love', 'electronic'] 
+words = ['jyoti', 'enviroment', 'programming', 'coding', 
+         'python', 'mohini', 'love', 'electronic','science','condition'] 
  
 word = random.choice(words)
 # print(word)
-a=len(word)
-print("The secret word length is =" ,a)
+length=len(word)
+print("The secret word length is =" ,length)
 
 print("Guess the characters")
 guesses = ''
 lifeline = 6
- 
+chances=6
  
 while lifeline > 0:
     fail = 0
     for char in word:
         if char in guesses: 
-            print(char)
-             
+            print(char)     
         else: 
             print("_")
             fail= fail+1
-             
- 
     if fail== 0:
         print("You Win") 
         print("The word is: ", word) 
@@ -34,89 +31,11 @@ while lifeline > 0:
     guess = input("guess a character:")
     guesses += guess 
     if guess not in word:
-         
         lifeline= lifeline-1
+        print(IMAGES[chances-lifeline])
         print("Wrong")
         print("You have", + lifeline, 'more guesses')
-         
-         
         if lifeline== 0:
             print("You Loose")
 
-
-if guess==0:
-    print('''
-
- 
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-        =========''')
-elif guess==1:
-    print('''
-    +---+
-    |   |
-    0   |
-        |
-        |
-        |
-        =========''')
-elif guesses==2:
-    print('''
-    +---+
-    |   |
-    0   |
-   /    |
-        |
-        |
-        =========''')
-elif guess==3:
-    print('''
-    +---+
-    |   |
-    0   |
-   /|   |
-        |
-        |
-        =========''')
-elif guesses==4:
-    print('''
-    +---+
-    |   |
-    0   |
-   /|\  |
-        |
-        |
-        =========''')
-elif guess==5:
-    print('''
-    +---+
-    |   |
-    0   |
-   /|\  |
-    |   |
-        |
-        =========''')
-elif  guesses==6:
-    print('''
-    +---+
-    |   |
-    0   |
-   /|\  |
-    |   |
-   /    |
-        =========''')
-elif guess==7:
-    print('''
-    +---+
-    |   |
-    0   |
-   /|\  |
-    |   |
-   / \  |
-        =========''' )
-	
 
